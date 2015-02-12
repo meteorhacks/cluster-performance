9587fc01eccabac1d62628b030acebab30a36f46
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-02-12 12:32:20 +0530

    multiple entry points

--- load_test.js
+++ load_test.js
@@ -4,8 +4,17 @@ meteorDown.init(function (Meteor) {
   });
 });
 
+var entryPoints = [
+  'http://104.236.246.62',
+  'http://104.236.216.226',
+  'http://104.131.182.168'
+];
+
+var pointIndex = Math.floor(Math.random() * 3);
+var entryPoint = entryPoints[pointIndex];
+
 meteorDown.run({
   concurrency: 10,
   // url: 'http://localhost:8080'
-  url: 'http://104.236.246.62'
+  url: entryPoint
 });
