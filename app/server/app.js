e26f4896751e11583e5fe6a1337bbe7c88a2e975
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-02-23 19:22:06 +0530

    Add passenger run script and cache support

--- app/server/app.js
+++ app/server/app.js
@@ -1,5 +1,6 @@
 var id = 0;
 var dataSizeInKb = parseInt(process.env.DATA_SIZE_IN_KB) || 200;
+var cacheDataLocally = !!process.env.CACHE_DATA_LOCALLY;
 Posts = new Mongo.Collection('posts');
 
 if(!Posts.findOne()) {
@@ -12,8 +13,12 @@ Meteor.publish("posts", function() {
   this.unblock();
   var query = {
     // make this query a new one every time
-    _id: {$ne: ++id}
+    // _id: {$ne: ++id}
   };
+
+  if(!cacheDataLocally) {
+    query['_id'] = {$ne: ++id};
+  }
 
   return Posts.find(query, {sort: {_id: 1}, limit: dataSizeInKb});
 });
--- run-passenger.sh
+++ run-passenger.sh
@@ -3,4 +3,5 @@ ln -s main.js app.js
 source ../config/env.sh
 stop meteor || :
 
+export CLUSTER_WORKERS_COUNT=0
 passenger start --port 80 --min-instances=4
